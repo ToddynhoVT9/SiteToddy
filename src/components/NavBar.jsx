93624bebf3852db3ee19319eec5950263e595788
ToddynhoVT9 <mateuslopes9954@gmail.com>
2026-02-15 13:56:03 -0300

    navbar compressed react module

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,47 +1,33 @@
 import { NavLink } from "react-router-dom";
 import logo from "../assets/logo.png";
 
-export default function Navbar() {
+function LinkItemNav({ to, text, className = "" }) {
   const baseStyle = "px-4 py-2 rounded transition-colors duration-200";
 
+  return (
+    <NavLink
+      to={to}
+      className={({ isActive }) =>
+        `${baseStyle} ${
+          isActive ? "bg-[#202020]" : "bg-[#545454] hover:bg-[#beb8b8]"
+        } ${className}`
+      }
+    >
+      {text}
+    </NavLink>
+  );
+}
+
+export default function Navbar() {
   return (
     <nav className="bg-[#7d7d7d] text-white py-4 px-6 flex gap-4 items-center">
       <NavLink to="/" className="flex items-center">
         <img src={logo} alt="Logo" className="h-10 w-auto" />
       </NavLink>
 
-      <NavLink
-        to="/blog"
-        className={({ isActive }) =>
-          `${baseStyle} ${
-            isActive ? "bg-[#202020]" : "bg-[#545454] hover:bg-[#beb8b8]"
-          }`
-        }
-      >
-        Blog
-      </NavLink>
-
-      <NavLink
-        to="/portfolio"
-        className={({ isActive }) =>
-          `${baseStyle} ${
-            isActive ? "bg-[#202020]" : "bg-[#545454] hover:bg-[#beb8b8]"
-          }`
-        }
-      >
-        portfólio
-      </NavLink>
-
-      <NavLink
-        to="/profile"
-        className={({ isActive }) =>
-          `${baseStyle} ${
-            isActive ? "bg-[#202020]" : "bg-[#545454] hover:bg-[#beb8b8]"
-          } ml-auto`
-        }
-      >
-        Perfil
-      </NavLink>
+      <LinkItemNav to="/blog" text="Blog" />
+      <LinkItemNav to="/portfolio" text="Portfólio" />
+      <LinkItemNav to="/profile" text="Perfil" className="ml-auto" />
     </nav>
   );
 }
